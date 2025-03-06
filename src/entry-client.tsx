41610dc83ad8563b395aaa49912387198a004db9
Mohammad <mohammadpoorjamal@gmail.com>
2025-03-06 15:18:48 +0330

    chore(package): add axios and tanstack query

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,10 +1,15 @@
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 hydrateRoot(
   document.getElementById("root")!,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
